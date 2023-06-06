ab87d2e60c86d797c52fd56fe549b7e82f6f8c5b
lucasfjoaquim <45808571+lucasfjoaquim@users.noreply.github.com>
2023-06-06 18:26:11 -0300

    key change and erro prevention

--- voice.py
+++ voice.py
@@ -3,13 +3,14 @@ import speech_recognition as sr
 import openai
 import pyttsx3
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Cria um reconhecedor
 engine = pyttsx3.init()
 r = sr.Recognizer()
 
 
+# speach_recognition
 def microfone():
     # Captura o áudio do microfone
     with sr.Microphone() as source:
@@ -25,9 +26,16 @@ def microfone():
     except sr.RequestError as e:
         print("Erro ao solicitar resultados do serviço do Google Speech Recognition; {0}".format(e))
 
-    perguntar_para_gpt(frase)
+    try:
+        perguntar_para_gpt(frase)
+    except Exception as E:
+        print(f"erro de leitura: {E}")
+        engine.say("Houve um erro na leitura da sua voz, por favor repita")
+        engine.runAndWait()
 
 
+
+# GPT
 def gerar_resposta(messages):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -73,22 +81,21 @@ def treinar_gpt(pergunta):
 
 
 treinar_gpt(
-    "voce deve comportar como um assistente virtual de uma firma de advocacioa, voce deve deve saber responder apenas perguntar do ramo, em caso de uma pergunta fora do ramo voce deve responder: não sei te informar, sou apenas um assistente de advocacia, e deve usar como referencia o codigo penal brasileiro e casos judiciais importantes, suas respostas devem ter no maximo 300 caracteres")
+    "voce deve comportar como um assistente virtual de uma firma de advocacioa,"
+    " voce deve deve saber responder apenas perguntar do ramo,"
+    " em caso de uma pergunta fora do ramo voce deve responder :"
+    " não sei te informar, sou apenas um assistente de advocacia,"
+    " e deve usar como referencia o codigo penal brasileiro e casos judiciais importantes,"
+    " suas respostas devem ter no maximo 300 caracteres")
 
-# Cria a janela
+# tkinter
 janela = tkinter.Tk()
 
-# Define o tamanho da janela e a cor de fundo
-janela.geometry("400x300")
+janela.geometry("300x150")
 janela.configure(bg="#333333")
 
-# Define um estilo para o botão
-
-# Cria o botão com o estilo personalizado
 botao = tkinter.Button(janela, text="Começe a falar", command=microfone, bg='#003366', font=('Helvetica', 12))
 botao.pack(pady=20)
 botao.configure(width=20)
 
-
-# Inicia o loop principal da interface
 janela.mainloop()
